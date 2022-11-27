e5dc3c52e3b97de4fdf33b933838327a984805f2
Bart Voet <bart_voet@telenet.be>
2022-11-27 01:28:03 +0100

    Move demo-code to separate module

--- sineplotter.py
+++ sineplotter.py
@@ -127,13 +127,3 @@ class PlotterMovieWriter:
                 plt.gcf().clear()
                 p.draw((float(i)  / float(self.frames)))
                 writer.grab_frame()
-
-period = 4
-axis_length = period * 2
-
-PlotterMovieWriter(frames = 300, fps=5) \
-    .writeMovie("sine.mp4",
-        PhasePlotter(axis_length = axis_length) \
-            .addSine(amp = 1.5, phase = -np.pi/4, period = period, color = 'r')
-            .addSine(amp = 2.5, phase =     0   , period = period, color = 'g')
-            .addSine(amp = 2.25,phase =  np.pi/2, period = period, color = 'b'))
---
+++ sineplotter_demo.py
@@ -0,0 +1,12 @@
+from sineplotter import PlotterMovieWriter, PhasePlotter
+import numpy as np
+
+period = 4
+axis_length = period * 2
+
+PlotterMovieWriter(frames = 300, fps=5) \
+    .writeMovie("sine.mp4",
+        PhasePlotter(axis_length = axis_length) \
+            .addSine(amp = 1.5, phase = -np.pi/4, period = period, color = 'r')
+            .addSine(amp = 2.5, phase =     0   , period = period, color = 'g')
+            .addSine(amp = 2.25,phase =  np.pi/2, period = period, color = 'b'))
